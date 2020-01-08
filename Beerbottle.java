38ddd26008cad93a0b780ea21df89a7f67023c56
Chandan kumar Jha <36193256+ckjha017@users.noreply.github.com>
2020-01-08 08:06:25 +0530

    Create Beerbottle.java

---
+++ Beerbottle.java
@@ -0,0 +1,14 @@
+public class Beerbottle {
+	 public static void main (String[] args) {
+		 int x = 99;
+		 while (x >= 1) {
+			 System.out.println (x + "bottles of beer on the wall,"  + x + "bottles of beer.");
+			 x = x-1;
+			 System.out.println("Take one down and pass it around," + x + "bottles of beer on the wall");
+		 }
+		 System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+		 System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
+		 
+	 }
+
+}
